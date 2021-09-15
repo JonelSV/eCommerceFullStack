25ff96ab84d09f726ef9dddaba89f5d3ab099741
JonelSV <svjonel@gmail.com>
2021-09-15 11:32:10 -0400

    'footer bootstrap'

---
+++ fullstackfront/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className='text-center py-3'>
+                 myFooter
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
