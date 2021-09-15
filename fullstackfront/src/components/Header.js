ad6924b39919818975a9793a87b811304266510f
JonelSV <svjonel@gmail.com>
2021-09-15 11:31:52 -0400

    'header bootsrap'

---
+++ fullstackfront/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='light' variant='light'  expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>My eCommerce Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'> <i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+              <Nav.Link href='/login'> <i className='fas fa-user'></i>Sign In</Nav.Link>
+              
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
