1e9922a714f9bf27c7f7a834417f46d661cacb2d
JonelSV <svjonel@gmail.com>
2021-09-15 11:32:38 -0400

    'bootstrap css'

--- fullstackfront/src/App.js
+++ fullstackfront/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header />
+      <main className='py-4'>
+        <Container>
+
+
       <h2>myEcommerce site</h2>
+
+        </Container>
+      </main>
+      <Footer />
+
+
+
     </div>
   );
 }
