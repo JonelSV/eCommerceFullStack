c86ff01d56c42488308b96128419027d8b8a1e62
JonelSV <svjonel@gmail.com>
2021-09-16 15:12:25 -0400

    'updated store'

--- fullstackfront/src/store.js
+++ fullstackfront/src/store.js
@@ -1,16 +1,22 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux' 
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-
+import {productListReducer} from './reducers/productReducers'
 
 // import devtools
 // create MAIN reducer - combine reducer {}, initialState - object
 
 
-const reducer = combineReducers({})
+const reducer = combineReducers({productList: productListReducer})
 
 const initialState = {}
 
 const middleware = [thunk]
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware([...middleware])))
+const store = createStore(
+        reducer,
+        initialState, 
+        composeWithDevTools(applyMiddleware(...middleware)))
+
+
+export default store
