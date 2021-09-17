d75aaf551ed0588b3231c7b8a3089bdf0c34f67c
JonelSV <svjonel@gmail.com>
2021-09-17 09:18:29 -0400

    'load screen'

---
+++ fullstackfront/src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Spinner } from 'react-bootstrap'
+
+const Loader = () => {
+    return (
+       <Spinner animation='border' 
+       role='status' 
+       style={{width: '100px', height: }}
+    )
+}
+
+export default Loader
