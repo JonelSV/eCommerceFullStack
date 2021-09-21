f0e7a833fece43c300bc3818865ebafda17a4b2b
JonelSV <svjonel@gmail.com>
2021-09-20 20:58:34 -0400

    'commit'

--- fullstackfront/src/weather/Weather.js
+++ fullstackfront/src/weather/Weather.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import axios from 'axios';
+
 
 const Weather = () => {
     return (
   <div>
-    	<Card style={{ width: '18rem' }}>
-				<Card.Img variant="top" src="https://picsum.photos/100/100" />
-				<Card.Body>
-					<Card.Title>Card Title</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the bulk of
-						the card's content.
-    				</Card.Text>
-					<Button variant="primary">Go somewhere</Button>
-				</Card.Body>
-			</Card>
+    	
   </div>
     )
 };
