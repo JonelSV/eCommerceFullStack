cd01d70ca06df0261afcc0689e63c84549648b16
JonelSV <svjonel@gmail.com>
2021-09-24 21:38:03 -0400

    'cart updated'

--- fullstackfront/src/screens/CartScreen.js
+++ fullstackfront/src/screens/CartScreen.js
@@ -5,11 +5,26 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import { addToCart } from '../actions/cartActions'
 
+// ? to get quantity=1 on browser, use location .search
+// does not give number format.
+// split sets = as an array in 0 index and 1 in 1 index
+
 const CartScreen = ({match, location, history}) => {
     const productId = match.params.id 
  
-    const quantity = location.search
-    console.log(quantity)
+    const quantity = location.search ? Number(location.search.split('=')[1]) : 1
+    const dispatch = useDispatch()
+    const cart = useSelector(state => state.cart)
+    const { cartItems } = cart
+    
+    // console.log(cartItems)
+
+    useEffect(() => {
+        if(productId){
+            dispatch(addToCart(productId, quantity))
+        }
+    }, [dispatch, productId, quantity])
+    // console.log(quantity) 
     return (
         <div>
            <h3>
