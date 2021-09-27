e3007650ff56cece242d75c2b03473029ff1616d
JonelSV <svjonel@gmail.com>
2021-09-26 21:45:36 -0400

    'fixe?'

--- fullstackfront/src/reducers/cartReducers.js
+++ fullstackfront/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../constants/cartConstants"; 
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants"; 
 
 
 // state takes items in the cart and store it as an array.
@@ -15,12 +15,12 @@ export const cartReducer = (state = {cartItems: []}, action) => {
     switch(action.type) {
         case CART_ADD_ITEM:
             const item = action.payload
-            const existItem = state.cartItems.find(itemId => itemId.tour === item.tour)
+            const existItem = state.cartItems.find((itemId) => itemId.tour === item.tour)
 
             if(existItem) {
                 return {
                     ...state,
-                    cartItems: state.cartItems.map(x => x.tour === existItem.tour ? item : x)
+                    cartItems: state.cartItems.map((x) => x.tour === existItem.tour ? item : x),
                 }    
             } else {
                 return {
@@ -28,7 +28,7 @@ export const cartReducer = (state = {cartItems: []}, action) => {
                     cartItems: [...state.cartItems, item]
                 }
             }
-        case CART_ADD_ITEM:
+        case CART_REMOVE_ITEM:
             return {
                 ...state,
                 cartItems: state.cartItems.filter((x) =>  x.tour !== action.payload),
