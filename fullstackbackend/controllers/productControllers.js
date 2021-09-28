0dc05d6abfa3ec23f7eea507c7dcefb5c19810a1
JonelSV <svjonel@gmail.com>
2021-09-27 21:01:01 -0400

    'controller'

--- fullstackbackend/controllers/productControllers.js
+++ fullstackbackend/controllers/productControllers.js
@@ -1,8 +1,26 @@
 import expressAsyncHandler from "express-async-handler";
 import Product from "../models/productModel";
 
+// @desc Fetch all tours
+// @route GET/api/tours
+// @access Public
+
+
 const getTours = asyncHandler(async (request, response) => {
     const tours = await Product.find({})
 
     response.json(tours)
 })
+const getTourById = asyncHandler(async (request, response) => {
+    
+   if (tour) {
+     response.json(tour);
+   } else {
+     res.status(404);
+     throw new Error('Product not found');
+   }
+})
+
+export {
+    getTours, getTourById
+}
